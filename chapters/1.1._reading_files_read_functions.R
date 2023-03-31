a3b2b6b17564c955bdc0f294e5ebc6091166f487
Christian <burkhart.christian@gmail.com>
2023-03-31 21:42:43 +0200

    1.1 finished

--- chapters/1.1._reading_files_read_functions.R
+++ chapters/1.1._reading_files_read_functions.R
@@ -5,8 +5,9 @@ mpg_new <- read_csv("data/mpg_uppercase.csv") |>
   clean_names() |> 
   select(manufacturer, model)
 
+
 read_csv("data/mpg_uppercase.csv",
-         name_repair = \(column) make_clean_names(column, case = "snake"),
-         col_select = c("manufacturer", "model")) |> 
-  glimpse()
+         name_repair = \(column_name) make_clean_names(column_name, 
+                                                       case = "snake"),
+         col_select = c(manufacturer, model))
 
